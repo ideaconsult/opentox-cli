3cae5a117ddea81268b5fbfbdf0aea37fa302ab7
ngn <jeliazkova.nina@gmail.com>
2015-05-31 10:36:43 +0300

    stress test

--- src/test/java/net/idea/opentox/cli/test/StressTest.java
+++ src/test/java/net/idea/opentox/cli/test/StressTest.java
@@ -2,11 +2,15 @@ package net.idea.opentox.cli.test;
 
 import java.net.URL;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import junit.framework.Assert;
 import net.idea.opentox.cli.OTClient;
+import net.idea.opentox.cli.qmrf.QMRFDocumentClient;
 import net.idea.opentox.cli.structure.Compound;
 import net.idea.opentox.cli.structure.CompoundClient;
 
@@ -29,51 +33,46 @@ public class StressTest extends
 	@Override
 	public void testRead() throws Exception {
 
-		int nt = 1000;
+		ExecutorService xs = Executors.newCachedThreadPool();
+		int nt = 100;
+
 		MyThread[] threads = new MyThread[nt];
 		for (int i = 0; i < nt; i++)
 			try {
 				threads[i] = new MyThread(String.format("thread-%d", i), 10000,
 						new OTClient(), new URL(
-								//"http://localhost:8080/ambit2/substance?type=like&search=*acid*"));
-								//"http://localhost:8080/ambit2/query/similarity?search=c1cccc1"));
-								//"http://localhost:8080/ambit2/bundle/29/substance"));
-								//"http://localhost:8080/ambit2/bundle/29/dataset"));
-								"http://localhost:8080/ambit2/dataset/1?max=4"));
-				threads[i].start();
+						// "http://localhost:8080/ambit2/substance?type=like&search=*acid*"));
+						// "http://localhost:8080/ambit2/query/similarity?search=c1cccc1"));
+						// "http://localhost:8080/ambit2/bundle/29/substance"));
+						// "http://localhost:8080/ambit2/bundle/29/dataset"));
+						// "http://localhost:8080/ambit2/dataset/1?max=40"
+								"http://localhost:8081/qmrf/protocol?max=40"));
 			} catch (Exception x) {
 				logger.log(Level.SEVERE, "loop", x);
 			}
-		logger.log(Level.INFO, "Waiting threads to join");
 		for (int i = 0; i < nt; i++)
-			try {
-				try {
-					threads[i].join();
-					logger.log(Level.INFO, String.format("JOINED %s %d %s", threads[i].toString(),threads[i].getCount(),threads[i].error==null?"":threads[i].error.getMessage()));
-				} catch (InterruptedException x) {
-					logger.log(Level.SEVERE, "interrupted", x);
-				}
-			} catch (Exception x) {
-				logger.log(Level.SEVERE, "join loop", x);
-			}
-		logger.log(Level.INFO, "completed");
+			xs.submit(threads[i]);
+		xs.awaitTermination(30, TimeUnit.SECONDS);
+		xs.shutdown();
+		int errors = -1;
 		long time = 0;
-		String s = "";
-		int errors = 0;
+		long maxtime = 0;
 		for (int i = 0; i < nt; i++) {
-			time += threads[i].time;
-			s += "\n" + threads[i].time;
-			if (threads[i].error!=null) {
-				errors++;
-				logger.log(Level.SEVERE,
-						String.format("%d. %s %s",(i),threads[i].getName(),threads[i].error));
-				//Assert.assertNotNull(threads[i].getName(), threads[i].dataset);
+			if (i == 0) {
+				time = 0;
+				errors = 0;
 			}
+			if (threads[i].getError() != null) {
+				errors++;
+				logger.log(Level.INFO,threads[i].getError().getMessage());
+			}	
+			if (maxtime < threads[i].time) maxtime = threads[i].time;
+			time += threads[i].time;
 		}
-		//logger.log(Level.INFO, s);
-		logger.log(Level.INFO,
-				"Average time per thread " + Long.toString(time / nt) + " ms.");
-		Assert.assertEquals(0,errors);
+
+		logger.log(Level.INFO,String.format(
+				"Time per thread:\tAverage %d\tMax %d , ms.",(time / nt),maxtime));
+		Assert.assertEquals(0, errors);
 	}
 
 	class MyThread extends Thread {
@@ -82,6 +81,11 @@ public class StressTest extends
 		public URL dataset;
 		public OTClient client;
 		public Exception error = null;
+
+		public Exception getError() {
+			return error;
+		}
+
 		protected Logger threadLogger;
 		protected int count = -1;
 
@@ -100,19 +104,25 @@ public class StressTest extends
 		@Override
 		public void run() {
 			time = System.currentTimeMillis();
-			CompoundClient cli = null;
-			//SubstanceClient cli = null;
+			QMRFDocumentClient cli = null;
+			// SubstanceClient cli = null;
 			try {
-				//cli = client.getSubstanceClient();
-				cli = client.getCompoundClient();
+				// cli = client.getSubstanceClient();
+				cli = client.getQMRFDocumentClient();
 				List list = cli.getJSON(dataset);
 				if (list == null || list.isEmpty()) {
-					threadLogger.log(Level.WARNING, "Empty list");
+					error = new Exception("Empty list");
+					//threadLogger.log(Level.WARNING, "Empty list");
 					count = 0;
-				} else count = list.size();
+				} else
+					count = list.size();
+				threadLogger.log(Level.INFO, String.format(
+						"COMPLETED %s %d %s", getName(), count,
+						error == null ? "" : error.getMessage()));
 			} catch (Exception x) {
 				error = x;
 				threadLogger.log(Level.SEVERE, this.getName(), x);
+
 			} finally {
 				time = System.currentTimeMillis() - time;
 				try {
